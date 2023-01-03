1f557cae5ee0545cb832a77ff2eaf79bd54e4e55
bemxio <bemxiov@protonmail.com>
2023-01-03 17:17:45 +0100

    add the first version of the site

---
+++ script.js
@@ -0,0 +1,9 @@
+const titles = [
+    "jebac Kurwy Szmaty puerdolone",
+    "jebac kurwy jebac",
+    "EEEOOOEEEOOOEOEOEOE",
+    "bende cie ruchal!!!!",
+    "jebac tom waszom spolecznosc >:("
+];
+
+document.title = titles[Math.floor(Math.random() * titles.length)];
