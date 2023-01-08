c76b7ef2f62446cefb30939f0ba80fc01c103adf
bemxio <bemxiov@protonmail.com>
2023-01-08 17:36:51 +0100

    make the website redirect people to various polish sites now

--- script.js
+++
@@ -1,9 +0,0 @@
-const titles = [
-    "jebac Kurwy Szmaty puerdolone",
-    "jebac kurwy jebac",
-    "EEEOOOEEEOOOEOEOEOE",
-    "bende cie ruchal!!!!",
-    "jebac tom waszom spolecznosc >:("
-];
-
-document.title = titles[Math.floor(Math.random() * titles.length)];
